6e60a234fb1f3b424b625b9f5cb0636fc9daa900
Anas Mofleh <49335621+AnasMofleh@users.noreply.github.com>
2024-11-14 23:38:21 +0100

    Update data_writer.py

--- actions/data_writer.py
+++ actions/data_writer.py
@@ -1,4 +1,5 @@
 from collections import OrderedDict
+import random
 import yaml
 
 
@@ -7,14 +8,21 @@ class data_writer:
         pass
 
     def update_info(self, current_data, path, key):
-        
+
         # Read YAML file
         with open(path, 'r') as file:
             data = yaml.safe_load(file)
 
+            current = OrderedDict()
+            for idx, e in enumerate(data[key]):
+                current[e['name'].lower().strip()] = idx
             for e in current_data:
-                data[key].update(current_data[e])
+                if e.lower().strip() in current:
+                    data[key][current[e.lower().strip()]] = current_data[e]
+                else:
+                    data[key].append(current_data[e])
 
+            random.shuffle(data['skills'])
             with open(path, 'w', encoding='utf8') as file:
                 yaml.dump(data, file, default_flow_style=False, allow_unicode=True, sort_keys=False)   
 
